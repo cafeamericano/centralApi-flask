71d61db8a8c44b9260993b804cad143564a8dd63
Matthew <matthew-dev@Matthews-MacBook-Air.local>
2020-03-08 19:52:56 -0400

    Soft coded port

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 # Imports
 from flask import Flask, Blueprint, jsonify
+import os
 
 # Define app variable
 app = Flask(__name__)
+
+# Define port on which to listen
+port = int(os.environ.get("PORT", 5000))
 
 # Import route files
 from apis.globeMonitor import GlobeMonitorAPI
@@ -12,9 +16,10 @@ app.register_blueprint(GlobeMonitorAPI)
 
 # Root URL Route
 @app.route('/')
-def hello_whale():
+def home():
     return '<h2>Centralized API - Flask</h2><p>Version 0.1</p><p>The server is listening for requests.</p>'
 
 # Start server listening
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=port)
+
